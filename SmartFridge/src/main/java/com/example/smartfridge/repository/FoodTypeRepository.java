ceaea7ec1f45290604ba323947550ba7c3a902a3
Michal Kalina <93913788+Mkali23@users.noreply.github.com>
2022-07-22 09:08:24 +0200

    Delete SmartFridge directory

--- SmartFridge/src/main/java/com/example/smartfridge/repository/FoodTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartfridge.repository;
-
-import com.example.smartfridge.models.FoodType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodTypeRepository extends JpaRepository<FoodType, Integer> {
-}
